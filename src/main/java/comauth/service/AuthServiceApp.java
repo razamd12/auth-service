9d1946e5bce9566e67f3dc34c3397885455494f7
razamd12 <razamuhammad.dahri@systemsltd.com>
2022-05-18 19:51:05 +0500

    Added my project

---
+++ src/main/java/comauth/service/AuthServiceApp.java
@@ -0,0 +1,13 @@
+package comauth.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApp.class, args);
+	}
+
+}
